4fdba8a1c01c1f72a9106ada66c30512618168c4
FusRoDahh <67054798+FusRoDahh@users.noreply.github.com>
2024-09-10 21:35:02 +0200

    creazione mondo, player, gestione camera e collisioni

---
+++ TomsAdventure/src/main/GamePanel.java
@@ -0,0 +1,83 @@
+package main;
+
+import entity.Player;
+import tile.Tile;
+import tile.TileManager;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    //SCREEN SETTINGS
+    final int originalTileSize = 16; //16x16 pixel
+    final int scale = 4;
+
+    public final int tileSize = originalTileSize * scale; //64x64 pixel
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol;  //1024 pixel
+    public final int screenHeight = tileSize * maxScreenRow; //768 pixel
+
+    //WORLD PARAMETER
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
+
+    int fps = 60;
+
+    TileManager tile = new TileManager(this);
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    public CollisionChecker cChecker = new CollisionChecker(this);
+    public Player player = new Player(this, keyH);
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/fps;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+            delta += (currentTime-lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    public void update() {
+        player.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        tile.draw(g2);
+        player.draw(g2);
+
+        g2.dispose();
+    }
+}
